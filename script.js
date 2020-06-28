9b627d32ad08ca9794b82bb6a10969b60d5ef3a5
Ceora Ford <ceoraford@Ceoras-MBP.home>
2020-06-28 14:52:08 -0400

    Change line 5 + 6 to match class of black and white keys. didn't fix anything tho

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const WHITE_KEYS = ['z', 'x', 'c', 'v', 'b', 'n', 'm']
 const BLACK_KEYS = ['s', 'd', 'g', 'h', 'j']
 
 const keys = document.querySelectorAll('.key')
-const whiteKeys = document.querySelectorAll('.key.white')
-const blackKeys = document.querySelectorAll('.key.black')
+const whiteKeys = document.querySelectorAll('.key.white-key')
+const blackKeys = document.querySelectorAll('.key.black-key')
 
 keys.forEach(key => {
   key.addEventListener('click', () => playNote(key))
